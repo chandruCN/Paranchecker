a6891c2058168aa4fcc58fdef51ca2ba9abee6e6
chandruCN <126045844+chandruCN@users.noreply.github.com>
2023-04-08 12:46:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+//create a variable from class name
 
 var input = document.querySelector(".inputBox");
 var clickbtn = document.querySelector(".check");
@@ -6,27 +7,31 @@ var finalValue = document.querySelector(".finalValue");
 var searchValue;
 var split_string = [];
 var Result;
-
+//for input value
 input.addEventListener("input", updateValue);
 clickbtn.addEventListener("click", checkinuputs);
 
 // Function for updating value
 function updateValue(e) {
   searchValue = e.target.value;
+  //string into an array of substrings
   split_string = searchValue.split("");
 }
-
+//checkinputsn
 function checkinuputs() {
   Result = inputsave(split_string);
   finalValue.innerHTML = Result;
 }
 
 function inputsave(split_string) {
+  //empty arr
   var stack = [];
   for (var i = 0; i < split_string.length; i++) {
+    //condition for getinput
     let x = split_string[i];
     if (x == "{" || x == "(" || x == "[" || x == "]" || x == "}" || x == ")") {
       if (isOpeningBrace(split_string[i])) {
+        //add neew items at end of the array
         stack.push(split_string[i]);
       } else if (closingBracesMatch(split_string[i], stack)) {
         stack.pop();
@@ -37,6 +42,7 @@ function inputsave(split_string) {
   }
   return stack.length === 0;
 }
+//function for closing bracket matching (boolean)
 
 function closingBracesMatch(closing, stack) {
   var opening = stack[stack.length - 1];
@@ -47,6 +53,7 @@ function closingBracesMatch(closing, stack) {
     (opening === "{" && closing === "}")
   );
 }
+//for openbrace (boolean)
 
 function isOpeningBrace(character) {
   return character === "(" || character === "{" || character === "[";
